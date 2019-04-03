93a9621cb3b77769c31f7ac002929f1c1fd03569
Julien-Broyard <broyard.dev@gmail.com>
2019-04-03 12:39:21 +0200

    :sparkles: Add cors middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 // Imports
 import bodyParser from "body-parser";
 import compression from "compression";
+import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 
@@ -19,6 +20,7 @@ class Server {
   private configureMiddlewares(): void {
     this.app.use(bodyParser.urlencoded({ extended: false }));
     this.app.use(bodyParser.json());
+    this.app.use(cors()); // TODO Set the origin later
     this.app.use(helmet());
     this.app.use(compression({ level: 6 }));
   }
