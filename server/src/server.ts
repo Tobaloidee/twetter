c46cb7865cef143db50633b34f0d336f76f5950a
Julien-Broyard <broyard.dev@gmail.com>
2019-04-04 17:54:27 +0200

    :sparkles: Add express-pino-logger

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@
 import bodyParser from "body-parser";
 import compression from "compression";
 import cors from "cors";
+import expressPino from "express-pino-logger";
 import express from "express";
 import helmet from "helmet";
 import mongoose from "mongoose";
@@ -50,6 +51,7 @@ class Server {
   }
 
   private configureMiddlewares(): void {
+    this.app.use(expressPino({ logger: Logger }));
     this.app.use(bodyParser.urlencoded({ extended: false }));
     this.app.use(bodyParser.json());
     this.app.use(cors()); // TODO Set the origin later
