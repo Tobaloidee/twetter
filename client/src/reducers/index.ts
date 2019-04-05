ef31cf82b4d8db0624cd2feb3b8f4bdb33bc2d5a
Julien-Broyard <broyard.dev@gmail.com>
2019-04-05 16:43:01 +0200

    :sparkles: Add auth reducer

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -2,8 +2,10 @@
 import { combineReducers } from "redux";
 
 // Reducers
+import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 
 export default combineReducers({
+  auth: authReducer,
   errors: errorReducer
 });
