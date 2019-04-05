6b756dbb9d6ca0fa6cbf96e327db20278879b012
Julien-Broyard <broyard.dev@gmail.com>
2019-04-05 16:22:22 +0200

    :sparkles: Add client

---
+++ client/src/index.tsx
@@ -0,0 +1,12 @@
+// Imports
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Components
+import App from "./App";
+
+// PWA
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+serviceWorker.unregister();
